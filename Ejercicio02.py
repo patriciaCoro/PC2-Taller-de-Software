da85df16c31f5e2e20619f5e7b2670bfa4f51fca
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-25 08:36:33 -0500

    Ejercicio02

---
+++ Ejercicio02.py
@@ -0,0 +1,23 @@
+#Una historia de usuario es una reseña de las necesidades solicitadas 
+#para nuestro sistema
+
+#Sirve para tener listado todas las necesidades del usuario o cliente
+#además de que puede servir de criterios de evaluaciones del cumplimiento
+#de especificaciones de nuestro sistema
+
+#EJEMPLO DE HISTORIA DE USUARIO
+
+#CARGO: Profesor de matemáticas
+
+#NECESIDAD: Necesito una aplicación que sumar a los
+#números que ingreso 
+
+
+
+
+def Suma(a,c):
+    sum = n+b
+    print(sum)
+n=int(input("Ingrese un número:"))
+b= int(input("Ingrese un número:"))
+Suma(n,b)
